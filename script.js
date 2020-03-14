ff9b6129f4b7daaceeeb06da5a70cb1ba15855fb
Sergei Zotov <zsv.main@gmail.com>
2020-03-14 08:56:37 +0500

    fefactor: change realize header

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let ulNavigation = document.querySelector("#navigation_ul");
-let liList = ulNavigation.querySelectorAll('li>a');
-liList.forEach(el => {
+let ulNavigation = document.querySelectorAll("#navigation_ul");
+ulNavigation.forEach(el => {
     el.addEventListener('click', (event) => {
-        liList.forEach(item => {
-            item.classList.remove('active');
-        })
-        event.target.classList.add('active');
+        if (event.target.classList.contains('a__href')) {
+            let listClickedLLink = document.querySelectorAll("#navigation_ul > li > a");
+            listClickedLLink.forEach(item => {
+                item.classList.remove('active');
+            })
+            event.target.classList.add('active');
+        }
     })
 })
+
+let phoneSlider = document.querySelector("body > main > div")
+document.querySelector("body > main > div > div.main_wrapper > a:nth-child(4) > div")
